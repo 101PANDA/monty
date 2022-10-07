a8a44b0aecea04a88d2f2cc56303dae15a9d7f07
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-10-07 04:02:58 +0000

    interpreter unfinished

---
+++ monty.h
@@ -0,0 +1,14 @@
+#ifndef __MONTY_H__
+#define __MONTY_H__
+
+#define _POSIX_C_SOURCE 200809L
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#endif
